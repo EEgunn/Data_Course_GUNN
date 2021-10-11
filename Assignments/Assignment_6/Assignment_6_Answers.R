fd8099f12bc3fb0b9f2ac4cbc49bc3488ed6627f
EEgunn <10287671@my.uvu.edu>
2021-10-11 06:13:07 -0600

    Assign6.1

--- Assignments/Assignment_6/Assignment_6_Answers.R
+++ Assignments/Assignment_6/Assignment_6_Answers.R
@@ -5,22 +5,29 @@ library(ggplot2)
 library(gganimate)
 
 
-setwd("./Data_class/Data_Course")
-dat <- read.csv("../../Data/BioLog_Plate_Data.csv")
+setwd("../../../Data_Course_GUNN")
+
+dat <- read.csv("./Assignments/Assignment_6/BioLog_Plate_Data.csv")
 glimpse(dat)
 
 
-dat_long <- dat %>% 
+#1 #2 #3 
+
+dat_long <- dat %>%  
   pivot_longer(cols = c("Hr_24","Hr_48","Hr_144"),
                names_to = "Time",
-               values_to = "Absorbance", names_prefix = "Hr_") %>% 
+               values_to = "Absorbance", names_prefix = "Hr_") %>%  
   arrange(desc(Dilution)) %>% 
   mutate(Sample_Type = case_when(Sample.ID == "Soil_1" ~ "Soil", 
                                  Sample.ID == "Soil_2" ~ "Soil", TRUE ~ "Water"))
 
+
+
 dat_long$Time <- as.numeric(dat_long$Time)
 
-Dil1 <- dat2[grepl("^0.1", dat2$Dilution),]
+
+
+Dil1 <- dat_long[grepl("^0.1", dat_long$Dilution),]
 
 
 Dil1 %>% 
@@ -31,11 +38,16 @@ Dil1 %>%
   facet_wrap(~Substrate,scales = "free")+
   theme_minimal()
 
+
+
+#4
+
 Ita <- dat_long[grepl("^Ita", dat_long$Substrate),]
 
 Ita2 <- Ita %>%
   group_by(Time,Sample.ID) %>% 
   mutate(Mean_Absorbance = mean(Absorbance))
+
   
 Ita2 %>% 
   group_by(Sample.ID) %>% 
