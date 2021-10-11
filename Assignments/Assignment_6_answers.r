faaa8ef88e64e066c62e9766e5ffdaffcedaf776
EEgunn <10287671@my.uvu.edu>
2021-10-11 09:53:52 -0600

    take3

--- Assignments/Assignment_6_answers.r
+++ Assignments/Assignment_6_answers.r
@@ -1,10 +1,49 @@
 library(tidyverse)
-library(palmerpenguins)
-library(dplyr)
-library(plotly)
-df <- penguins
+library(tidyr)
+library(ggpubr)
+library(ggplot2)
+library(gganimate)
 
-dat <- read_csv("../../Data/BioLog_Plate_Data.csv")
-# you may need to modify this path to fit where this 
-#file is stored relative to your assignment 6 script
+
+setwd("../../../Data_Course_GUNN/Assignments/Assignment_6")
+dat <- read.csv("BioLog_Plate_Data.csv")
+glimpse(dat)
+
+
+dat_long <- dat %>% 
+  pivot_longer(cols = c("Hr_24","Hr_48","Hr_144"),
+               names_to = "Time",
+               values_to = "Absorbance", names_prefix = "Hr_") %>% 
+  arrange(desc(Dilution)) %>% 
+  mutate(Sample_Type = case_when(Sample.ID == "Soil_1" ~ "Soil", 
+                                 Sample.ID == "Soil_2" ~ "Soil", TRUE ~ "Water"))
+
+dat_long$Time <- as.numeric(dat_long$Time)
+
+Dil1 <- dat_long[grepl("^0.1", dat_long$Dilution),]
+
+
+Dil1 %>% 
+  group_by(Substrate) %>% 
+  ggplot(aes(x=Time, y=Absorbance, color=Sample_Type))+ 
+  ylim(0.0,2.0)+
+  geom_smooth(se=FALSE)+
+  facet_wrap(~Substrate,scales = "free")+
+  theme_minimal()
+
+Ita <- dat_long[grepl("^Ita", dat_long$Substrate),]
+
+Ita2 <- Ita %>%
+  group_by(Time,Sample.ID) %>% 
+  mutate(Mean_Absorbance = mean(Absorbance))
+  
+Ita2 %>% 
+  group_by(Sample.ID) %>% 
+  ggplot(aes(x=Time, y= Mean_Absorbance, color=Sample.ID))+ 
+  geom_line()+
+  ylim(0.0,2.5)+
+  facet_wrap(~Dilution,scales = "free")+
+  theme_minimal() + 
+  transition_reveal(Time)
+
 
